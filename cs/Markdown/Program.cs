28b8ee363268a4225482c6c2efdfae412f7dce77
kokodio <kirill.zaripow@gmail.com>
2024-12-01 19:57:47 +0500

    update entrypoint

--- cs/Markdown/Program.cs
+++ cs/Markdown/Program.cs
@@ -1,12 +1,14 @@
 ﻿using Markdown;
+using Markdown.Converters;
+using Markdown.Tokenizers;
 
-var markdown = new Md();
+var tokenizer = new KonturMdTokenizer();
+var converter = new HtmlConverter();
+var markdown = new Md(tokenizer, converter);
 
-var input =
-    """
-    _123_!!
-    _123_
-    """;
+const string input = """
+                     Подчерки внутри текста c цифрами_12_3 не считаются выделением и должны оставаться символами подчерка.
+                     """;
 
 var result = markdown.Render(input);
 
