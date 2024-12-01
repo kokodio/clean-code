da0c7b9ae039af315ee5f3b51a747ef4fc11bdde
kokodio <kirill.zaripow@gmail.com>
2024-12-01 19:56:49 +0500

    add converter

---
+++ cs/Markdown/Converters/HtmlConverter.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Markdown.Converters;
+
+public class HtmlConverter : IConverter
+{
+    public string Convert(List<Token> tokens)
+    {
+        var html = new StringBuilder();
+        var isClosed = new Dictionary<TokenType, bool>
+        {
+            {TokenType.Italic, true},
+            {TokenType.Text, true},
+            {TokenType.Strong, true},
+        };
+
+        foreach (var token in tokens)
+        {
+            html.Append(token.Type switch
+            {
+                TokenType.Text => token.Content,
+                TokenType.Italic when token.Pair != null =>
+                    isClosed[token.Type] ? Tag.Open("em") : Tag.Close("em"),
+                TokenType.Strong when token.Pair != null  =>
+                    isClosed[token.Type] ? Tag.Open("strong") : Tag.Close("strong"),
+                _ => token.Content
+            });
+
+            if (token.Pair != null)
+            {
+                isClosed[token.Type] = !isClosed[token.Type];
+            }
+        }
+
+        return html.ToString();
+    }
+}
